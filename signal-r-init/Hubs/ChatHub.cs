eaa8261ce826e7323641804149cadf1934fcbaae
Samrat Shrestha <saamraat.shrestha@gmail.com>
2024-11-09 20:31:03 -0600

    initial commit

---
+++ signal-r-init/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace signal_r_init.Hubs
+{
+	public class ChatHub:Hub
+	{
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
+
